ad5274525ae44d46d3ef3ab61f3da020ed8a3c91
Suyash3385 <suyashsharma942@gmail.com>
2025-12-17 17:15:49 +0530

    [Suyash]: added the program of making a calculator

---
+++ gcr-codebase/programming-elements/level-2-practice-programs/Calculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Calculator{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+    //taking the input from the user
+    float number1=sc.nextInt();
+	float number2=sc.nextInt();
+	float addition=number1+number2;
+	float subtraction=number1-number2;
+	float multiplication=number1*number2;
+	float division=number1/number2;
+	
+       System.out.println(
+            "The addition, subtraction, multiplication, and division value of 2 numbers "
+            + number1 + " and " + number2 + " is "
+            + addition + ", " + subtraction + ", " + multiplication + ", and " + division
+        );
+     }
+}	 
+	
