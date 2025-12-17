e083425705b599d673d9914a95069f2f279ff940
Suyash3385 <suyashsharma942@gmail.com>
2025-12-17 16:37:38 +0530

    [Suyash] :  updated program-prerequisite folder

--- gcr-codebase/programming-elements/level-1-practice-programs/ConvertDistance.java
+++ gcr-codebase/programming-elements/level-1-practice-programs/ConvertDistance.java
@@ -1,7 +1,6 @@
 class ConvertDistance{
 public static void main(String[]args){
-int distance=10.8;
-int miles=0.6214;
-System,out.println("The distance 10.8km in miles is:" + (distance*miles));
+double distance=10.8, miles=0.6214;
+System.out.println("The distance 10.8km in miles is:" + (distance*miles));
 }
 }
