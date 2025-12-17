c1ea62e3c788524a90bbff800115a9a5f756fd6c
Suyash3385 <suyashsharma942@gmail.com>
2025-12-17 12:30:08 +0530

    [Suyash]: added FindAge program

---
+++ gcr-codebase/programming-elements/level-1-practice-programs/FindAge.java
@@ -0,0 +1,7 @@
+class FindAge{
+public static void main(String[]args){
+//find the age of Harry in the year 2024
+int birthYear=2000,currentYear=2024;
+System.out.println("Harry's age in 2024 is :"+ (currentYear-birthYear));
+}
+}
