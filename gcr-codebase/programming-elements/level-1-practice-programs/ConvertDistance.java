4a7ff895acfeca80f6ddb7607850110c6b52ef99
Suyash3385 <suyashsharma942@gmail.com>
2025-12-17 14:35:04 +0530

    [Suyash]: added the logic of converting kilometers to miles.

---
+++ gcr-codebase/programming-elements/level-1-practice-programs/ConvertDistance.java
@@ -0,0 +1,7 @@
+class ConvertDistance{
+public static void main(String[]args){
+int distance=10.8;
+int miles=0.6214;
+System,out.println("The distance 10.8km in miles is:" + (distance*miles));
+}
+}
