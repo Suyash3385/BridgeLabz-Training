489adc9872efe6046873f5276c37ca55eb9b4914
Suyash3385 <suyashsharma942@gmail.com>
2025-12-17 14:57:00 +0530

    [Suyash]: added the program of profit and loss

---
+++ gcr-codebase/programming-elements/level-1-practice-programs/ProfitAndLoss.java
@@ -0,0 +1,13 @@
+public class ProfitAndLoss{
+public static void main(String[]args){
+int costPrice=126,sellingPrice=191;
+int profit=sellingPrice-costPrice;
+double profitPercentage=((double)profit/costPrice*100);
+System.out.println("The Cost Price is INR 126 and Selling Price is INR 191\n" +
+"The Profit is INR "+profit + "and the Profit Percentage is"+profitPercentage);
+
+
+
+
+}
+}
