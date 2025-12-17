4734126f74dec1faf83115067fbb97a31f4d5aa0
Suyash3385 <suyashsharma942@gmail.com>
2025-12-17 16:53:30 +0530

    [Suyash] : added the logic of side of square

---
+++ gcr-codebase/programming-elements/level-1-practice-programs/PerimeterOfSquare.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class PerimeterOfSquare{
+  public static void main(String[]args){
+   //Take the scanner and make the object
+   Scanner sc=new Scanner(System.in);
+   double perimeter=sc.nextInt();
+    double side = perimeter / 4;
+	System.out.println(" The length of the side is" +sid+ " whose perimeter is"+ perimeter); 
+	   }
+}	  
+	
+	
+   
