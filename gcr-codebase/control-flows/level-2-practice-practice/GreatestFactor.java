6dd2a65ebf33d9e27ff772c66e3e9f924a31683d
Suyash3385 <suyashsharma942@gmail.com>
2025-12-18 20:48:07 +0530

    [suyash] :added the code of greatest factor using for loop

---
+++ gcr-codebase/control-flows/level-2-practice-practice/GreatestFactor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class GreatestFactor {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        // Check for positive integer
+        if (number <= 1) {
+            System.out.println("No greatest factor");
+        } else {
+
+            int greatestFactor = 1; 
+
+            // Loop from number-1 down to 1
+            for (int i = number - 1; i >= 1; i--) {
+                if (number % i == 0) {
+                    greatestFactor = i;
+					
+					//  exits the loop 
+                    break;  
+                }
+            }
+
+        
+            System.out.println( "Greatest factor of " + number + " excluding itself is: " + greatestFactor);
+        }
+    }
+}
