d07f9de07f92cb092b99bcf000f45ebee6f8183e
Suyash3385 <suyashsharma942@gmail.com>
2025-12-18 11:11:20 +0530

    [suyash] :added the logic of area of circle

---
+++ gcr-codebase/program-prerequisite/AreaOfCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AreaOfCircle{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int radius=sc.nextInt();
+int area= 22/7 * radius*radius;
+System.out.println(area);
+}
+}
