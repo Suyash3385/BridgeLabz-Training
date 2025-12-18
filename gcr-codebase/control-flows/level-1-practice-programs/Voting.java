5bcbe171da80469788407d399a0b62f72f740371
Suyash3385 <suyashsharma942@gmail.com>
2025-12-18 13:09:40 +0530

    [Suyash] : added the program whether a person is eligible to vote

---
+++ gcr-codebase/control-flows/level-1-practice-programs/Voting.java
@@ -0,0 +1,20 @@
+//Write a program to check whether a person can vote, depending on whether his/her age is greater than or equal to 18.
+ import java.util.Scanner;
+ public class Voting{
+    public static void main(String[]args){
+	
+	
+	Scanner sc=new Scanner(System.in);
+	int age =sc.nextInt();
+	
+	//check whether a person is 18 or above
+	if(age>=18){
+	System.out.println("The person's age is" + age + " and can vote");
+	}
+	else{
+	System.out.println("The person's age is " + age + " and cannot vote");
+	}
+
+
+	}
+}	
