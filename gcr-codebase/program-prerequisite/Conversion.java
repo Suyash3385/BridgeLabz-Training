7e4b9552d153ad37c8bc063503bcfe23dcbee84d
Suyash3385 <suyashsharma942@gmail.com>
2025-12-18 11:15:54 +0530

    [Suyash]: added the logic of conversion

---
+++ gcr-codebase/program-prerequisite/Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Conversion{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int celcius=sc.nextInt();
+int farhenheit=(celcius*9/5)+32;
+System.out.println(farhenheit);
+}
+}
