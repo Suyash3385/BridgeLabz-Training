94f137c884a052f979fbd5949c4a72d30cad38c1
Suyash3385 <suyashsharma942@gmail.com>
2025-12-19 15:07:29 +0530

    [Suyash] :added the code of day of week using command line input

---
+++ gcr-codebase/control-flows/level-3-practice-programs/DayOfWeek.java
@@ -0,0 +1,24 @@
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+
+         int m = Integer.parseInt(args[0]); 
+        int d = Integer.parseInt(args[1]); 
+        int y = Integer.parseInt(args[2]); 
+
+        // Calculate y0
+        int y0 = y - (14 - m) / 12;
+
+        //Calculate x
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+
+        //Calculate m0
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+
+        //Calculate d0 
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+
+       
+        System.out.println("Day of the week: " + d0);
+    }
+}
