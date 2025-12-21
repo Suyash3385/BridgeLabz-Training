b08704e0a9d472380bf972e16dfcbe5069903658
Suyash3385 <suyashsharma942@gmail.com>
2025-12-21 12:52:31 +0530

    [Suyash]:Added the code of Bmi

---
+++ gcr-codebase/Methods/level-2-practice-program/BMI.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class BMICalculator {
+   
+    public static void calculateBMI(double[][] data) {
+        for (int i = 0; i < data.length; i++) {
+             double weight = data[i][0];   
+             double heightCm = data[i][1]; 
+            double heightM = heightCm / 100; 
+
+         double bmi = weight / (heightM * heightM);
+          data[i][2] = bmi; // store BMI in third column
+      }
+    }
+
+   public static String[] determineBMIStatus(double[][] data) {
+         String[] status = new String[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            double bmi = data[i][2];
+            if (bmi < 18.5) {
+                status[i] = "Underweight";
+            } 
+			else if (bmi >= 18.5 && bmi < 24.9) {
+                status[i] = "Normal weight";
+            }
+			else if (bmi >= 25 && bmi < 29.9) {
+                status[i] = "Overweight";
+            } 
+			else {
+                status[i] = "Obese";
+          }
+        }
+           return status;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double[][] teamData = new double[10][3];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter weight (kg) for person " + (i + 1) + ": ");
+            teamData[i][0] = scanner.nextDouble();
+            System.out.print("Enter height (cm) for person " + (i + 1) + ": ");
+            teamData[i][1] = scanner.nextDouble();
+        }
+
+        // Calculate BMI 
+        calculateBMI(teamData);
+        String[] bmiStatus = determineBMIStatus(teamData);
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d\t%.2f\t\t%.2f\t\t%.2f\t%s\n",
+           i + 1, teamData[i][0], teamData[i][1], teamData[i][2], bmiStatus[i]);
+        }
+    }
+}
