438ee7515352f4b830bfb2f26bc87f1e74de6ba0
Suyash3385 <suyashsharma942@gmail.com>
2025-12-24 12:28:00 +0530

    [Suyash]:Added the code of gcd and lcm

---
+++ gcr-codebase/Extras/Built-in-functions/practice-program-2/GcdLcm.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class GcdLcm{
+    public static int findGCD(int a,int b) 
+	{
+        while(b!=0) {
+            int temp=b;
+            b=a%b;
+            a=temp;
+     }
+        return a;
+ }
+    public static int findLCM(int a,int b){
+        return (a*b)/findGCD(a, b);
+      }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+
+        int gcd=findGCD(num1, num2);
+        int lcm=findLCM(num1, num2);
+        System.out.println( gcd);
+        System.out.println( lcm);
+  }
+}
