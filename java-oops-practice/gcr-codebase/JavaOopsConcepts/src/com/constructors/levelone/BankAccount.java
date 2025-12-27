d2ae6805a2eab3f898ebe788761abdaec3f35974
Suyash3385 <suyashsharma942@gmail.com>
2025-12-27 21:26:56 +0530

    [Suyash]:added the bankaccount class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/BankAccount.java
@@ -0,0 +1,34 @@
+package com.constructors.levelone;
+
+public class BankAccount {
+    public long accountNumber;       
+    protected String accountHolder;   
+    private double balance;           
+
+    // Constructor
+    BankAccount(long accountNumber, String accountHolder, double balance) {
+        this.accountNumber=accountNumber;
+        this.accountHolder=accountHolder;
+        this.balance=balance;
+    }
+
+    //getter
+    public double getBalance(){
+        return balance;
+    }
+
+    public void deposit(double amount){
+        if (amount>0) {
+            balance+=amount;
+        }
+    }
+
+
+    public void withdraw(double amount){
+        if (amount>0 && amount<= balance) {
+            balance-=amount;
+        }
+    }
+
+
+}
