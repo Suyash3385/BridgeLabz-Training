2bf8c005933c8adc69307de7490cf97442cc4f29
Suyash3385 <suyashsharma942@gmail.com>
2025-12-27 17:14:39 +0530

    [Suyash]:Added the product class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/Product.java
@@ -0,0 +1,29 @@
+package com.constructors.levelone;
+
+class Product{
+
+  
+    String productName;
+    double price;
+
+    static int totalProducts=0;
+
+    // Constructor
+    Product(String productName, double price) {
+        this.productName=productName;
+        this.price=price;
+        totalProducts++; 
+    }
+
+    
+    void displayProductDetails() {
+        System.out.println("Product Name : " + productName);
+        System.out.println("Price        : " + price);
+    }
+
+   
+    static void displayTotalProducts() {
+        System.out.println("Total Products Created : " + totalProducts);
+    }
+
+}
