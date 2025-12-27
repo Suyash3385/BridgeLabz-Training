7ad7fd1a48d11d1e7348ccc5ca10f403061640e2
Suyash3385 <suyashsharma942@gmail.com>
2025-12-27 17:15:44 +0530

    [Suyash]:Added the main method of product class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/ProductMain.java
@@ -0,0 +1,26 @@
+package com.constructors.levelone;
+
+public class ProductMain {
+
+
+	    public static void main(String[] args) {
+
+	        Product p1=new Product("Laptop", 55000);
+	        Product p2=new Product("Mobile", 25000);
+	        Product p3=new Product("Headphones", 3000);
+
+	        p1.displayProductDetails();
+	        System.out.println();
+
+	        p2.displayProductDetails();
+	        System.out.println();
+
+	        p3.displayProductDetails();
+	        System.out.println();
+
+	        Product.displayTotalProducts(); 
+	    }
+	}
+
+
+
