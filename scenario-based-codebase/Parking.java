c459c4ce784c01ec43f1e9be518487621d39798d
Suyash3385 <suyashsharma942@gmail.com>
2025-12-29 15:09:28 +0530

    [Suyash]:Added the code of parking system

---
+++ scenario-based-codebase/Parking.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Parking{
+   public static void main(String[]args){
+   Scanner sc=new Scanner(System.in);
+   int spots=10; 
+   int occupied=0;
+   boolean exit=false;
+   
+   while(!exit){
+   System.out.println("choose");
+   System.out.println("park");
+   System.out.println("exit");
+   System.out.println("show occupancy");
+   
+   int choose=sc.nextInt();
+   
+   switch(choose){
+   case 1:
+   if(occupied<spots){
+   occupied++;
+   System.out.println("car parked successfully");
+   }
+   else{
+  System.out.println("car lot full");
+   }
+   break;
+   case 2:
+  
+    if(occupied>0){
+       occupied--;
+        System.out.println("Car exited, Available spots " + (spots - occupied));
+         }
+		 else {
+     System.out.println("Parking lot is empty");
+        }
+       break;
+    case 3:
+    System.out.println("Parking lot occupancy: " + occupied + spots);
+      break;
+
+    case 4:
+       System.out.println("Exiting program");
+        exit = true;
+       break;
+
+      default:
+     System.out.println("Invalid choice");
+            }
+   if(occupied==spots){
+   System.out.println("parking lot is full. no more cars allowed");
+   exit=true;
+   }
+   }
+   }
+   }
