2d793223268a600919af5976442bddd342299fb4
Suyash3385 <suyashsharma942@gmail.com>
2025-12-30 17:06:09 +0530

    [Suyash]:Added the department class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/Department.java
@@ -0,0 +1,17 @@
+package com.objectmodelling;
+
+
+	class Department {
+	    String departmentName;
+
+	    Department(String departmentName) {
+	        this.departmentName = departmentName;
+	    }
+
+	    void display() {
+	        System.out.println("Department: " + departmentName);
+	    }
+	}
+
+
+
