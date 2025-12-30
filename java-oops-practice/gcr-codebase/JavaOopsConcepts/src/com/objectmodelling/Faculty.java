0ebb38601093ca8fc5b6f69f9b7d6de5800a2ce0
Suyash3385 <suyashsharma942@gmail.com>
2025-12-30 17:08:50 +0530

    [Suyash]:Added the faculty class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodelling/Faculty.java
@@ -0,0 +1,17 @@
+package com.objectmodelling;
+
+public class Faculty {
+	
+	    String name;
+
+	    Faculty(String name) {
+	        this.name = name;
+	    }
+
+	    void display() {
+	        System.out.println("Faculty: " + name);
+	    }
+	}
+
+
+
