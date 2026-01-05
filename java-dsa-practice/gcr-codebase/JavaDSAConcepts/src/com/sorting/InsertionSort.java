e0b11e782f127d69d121269c24e04b7d26b6617e
Suyash3385 <suyashsharma942@gmail.com>
2026-01-05 22:35:50 +0530

    [Suyash]:Added the code of insertion sort

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.sorting;
+import java.util.*;
+public class InsertionSort {
+public static void sortEmployeeId(int arr[]) {
+	for(int i=0;i<arr.length;i++) {
+		int key=arr[i];
+		int j=i-1;
+		while(j>=0 && arr[j]>key) {
+			arr[j+1]=arr[j];
+			j--;
+		}
+		arr[j+1]=key;
+	}
+}
+public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int arr[]=new int[n];
+	for(int i=0;i<n;i++) {
+		arr[i]=sc.nextInt();
+	}
+	sortEmployeeId(arr);
+	System.out.println(Arrays.toString(arr));
+	
+}
+}
