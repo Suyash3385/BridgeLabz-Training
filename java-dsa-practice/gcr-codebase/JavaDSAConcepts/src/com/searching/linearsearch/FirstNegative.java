32a8b66f36b4d17060b2278792db228d1a70c398
Suyash3385 <suyashsharma942@gmail.com>
2026-01-07 11:50:00 +0530

    [Suyash]:Added the program of finding first negative index

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/searching/linearsearch/FirstNegative.java
@@ -0,0 +1,22 @@
+package com.searching.linearsearch;
+import java.util.*;
+public class FirstNegative {
+public static void main(String[]args) {
+	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
+	int arr[]=new int[n];
+	int index=-1;
+   for(int i=0;i<n;i++) {
+	   arr[i]=sc.nextInt();
+   }
+   for(int i=0;i<arr.length;i++) {
+	   if(arr[i]<0) {
+		  index=i;
+	      break;
+	   }
+
+	
+   }
+   System.out.println("index of the element is : "+ index);
+}
+}
