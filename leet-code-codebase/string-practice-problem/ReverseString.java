50d8b8ab9a1a643ee5da5f0203f857289613e04f
Suyash3385 <suyashsharma942@gmail.com>
2026-01-07 22:52:29 +0530

    [Suyash]:Added the reverse string problem

---
+++ leet-code-codebase/string-practice-problem/ReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input=sc.nextLine();
+        char[] s=input.toCharArray();
+        reverseString(s);  
+        System.out.println("Reversed string: " + new String(s));
+    }
+    public static void reverseString(char[] s){
+        int left=0;
+        int right=s.length-1;
+
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+     }
+    }
+    }
