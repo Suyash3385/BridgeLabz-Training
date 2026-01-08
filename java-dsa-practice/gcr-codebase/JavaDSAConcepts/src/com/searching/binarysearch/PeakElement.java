4300fa2530aa16b6b8871355a711b5730a6bded4
Suyash3385 <suyashsharma942@gmail.com>
2026-01-08 12:36:08 +0530

    [Suyash]:Added the code of finding peak element

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/searching/binarysearch/PeakElement.java
@@ -0,0 +1,24 @@
+package com.searching.binarysearch;
+import java.util.*;
+public class PeakElement {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++) {
+        	arr[i]=sc.nextInt();
+        }
+        int left=0,right=arr.length-1;
+        while(left<right) {
+        	int mid=(left+right)/2;
+        	 if(arr[mid] < arr[mid+ 1]) {
+        			left = mid + 1;
+        	}
+        	else {
+        		right=mid;
+        	}
+        }
+        System.out.println(arr[left]);
+	}
+
+}
