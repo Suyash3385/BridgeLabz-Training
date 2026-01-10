4c1ff42fe12ad859593aed22964cf8b244d26238
Suyash3385 <suyashsharma942@gmail.com>
2026-01-10 12:34:38 +0530

    [Suyash]:Added the BookShelf

--- java-dsa-practice/scenario-based/DsaScenarioPractice/src/com/BookShelf/Book.java
+++ java-dsa-practice/scenario-based/DsaScenarioPractice/src/com/BookShelf/Book.java
@@ -1,5 +1,31 @@
 package com.BookShelf;
-
+import java.util.*;
 public class Book {
+private String title;
+private String author;
+private String genre;
+Book(String title,String author,String genre){
+	this.title=title;
+	this.author=author;
+	this.genre=genre;
+	
+}
+public String getTitle() {
+	return title;
+}
+public String getAuthor() {
+	return author;
+	
+}
+public String getGenre() {
+	return genre;
+	
+}
 
+public String toString() {
+    return title + " by " + author;
+}
+public String getIdentifier() {
+    return title.toLowerCase() + "-" + author.toLowerCase();
+}
 }
