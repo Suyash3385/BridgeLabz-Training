acc1211367116b6b1473d44658273faff61f2313
Suyash3385 <suyashsharma942@gmail.com>
2026-01-23 23:50:50 +0530

    [Suyash]:Added the code of reverse queue

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/collections/queueinterface/ReverseQueue.java
@@ -0,0 +1,28 @@
+package com.collections.queueinterface;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+public class ReverseQueue {
+	public static void reverseQueue(Queue<Integer>queue) {
+		if(queue.isEmpty()) {
+			return;
+		}
+		int front=queue.remove();
+		reverseQueue(queue);
+		queue.add(front);
+	}
+	  public static void main(String[] args) {
+	        Scanner sc=new Scanner(System.in);
+	        Queue<Integer> queue=new LinkedList<>();
+	        System.out.print("Enter number of elements: ");
+	        int n=sc.nextInt();
+	        System.out.println("Enter queue elements:");
+	        for (int i=0;i<n;i++) {
+	            queue.add(sc.nextInt());
+	        }
+     System.out.println("Original Queue:" + queue);
+     reverseQueue(queue);
+     System.out.println("Reversed Queue:" + queue);
+
+}
+}
