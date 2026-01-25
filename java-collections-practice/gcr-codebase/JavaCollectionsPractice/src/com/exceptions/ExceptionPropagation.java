fbb45e8d001c3478ce2900fcb78bcc3e36a7acdb
Suyash3385 <suyashsharma942@gmail.com>
2026-01-25 15:01:47 +0530

    [Suyash]:Added the code of throw and throws

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exceptions/ExceptionPropagation.java
@@ -0,0 +1,24 @@
+package com.exceptions;
+
+public class ExceptionPropagation {
+	    public static double calculateInterest(double amount, double rate, int years)
+	            throws IllegalArgumentException {
+	        if (amount<0||rate<0) {
+	            throw new IllegalArgumentException("Invalid input");
+	        }
+	        return (amount*rate*years)/100;
+	    }
+	    public static void main(String[] args) {
+
+	        try {
+	            double interest=calculateInterest(10000, 5, 2);
+	            System.out.println("Calculated Interest: " + interest);
+	        } 
+	        catch (IllegalArgumentException e) {
+	            System.out.println("Invalid input");
+	        }
+	    }
+	}
+
+
+
