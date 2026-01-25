0f5e95a683cabb5e741dbfc809209d7f224d7df7
Suyash3385 <suyashsharma942@gmail.com>
2026-01-25 15:09:20 +0530

    [Suyash]:Added code of exception propagation using methods

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exceptions/ExceptionPropagationMethods.java
@@ -0,0 +1,22 @@
+package com.exceptions;
+
+public class ExceptionPropagationMethods {
+	    static void method1() {
+	        int result=10/0;
+	    }
+	    static void method2() {
+	        method1();          
+	    }
+	    public static void main(String[] args) {
+	        try {
+	            method2();        
+
+	        } 
+	        catch (ArithmeticException e) {
+	            System.out.println("Handled exception ");
+	        }
+	    }
+	}
+
+
+
