26c765b83e0acb9e8f1b75194f8fdb07bd758578
Suyash3385 <suyashsharma942@gmail.com>
2026-01-26 15:29:10 +0530

    [Suyash]:Added code of object to json format

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/ObjectToJson.java
@@ -0,0 +1,52 @@
+package com.reflection;
+
+	import java.lang.reflect.Field;
+	public class ObjectToJson {
+	    public static String toJson(Object obj) {
+	        if (obj==null) return "null";
+	        StringBuilder json=new StringBuilder();
+	        Class<?>cls=obj.getClass();
+	        json.append("{");
+	        Field[] fields=cls.getDeclaredFields();
+	        for (int i=0;i<fields.length;i++) {
+	            Field field=fields[i];
+	            field.setAccessible(true);
+	            try {
+	                json.append("\"").append(field.getName()).append("\":");
+	                Object value=field.get(obj);
+	                if (value instanceof String) {
+	                    json.append("\"").append(value).append("\"");
+	                } 
+	                else {
+	                    json.append(value);
+	                }
+
+	                if (i<fields.length-1) {
+	                    json.append(", ");
+	                }
+	            } 
+	            catch (IllegalAccessException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	        json.append("}");
+	        return json.toString();
+	    }
+	    static class Student {
+	        private int id;
+	        private String name;
+	        private double marks;
+	        Student(int id, String name, double marks) {
+	            this.id=id;
+	            this.name=name;
+	            this.marks=marks;
+	        }
+	    }
+	    public static void main(String[] args) {
+
+	        Student student=new Student(101, "Suyash", 92.5);
+	        String json=toJson(student);
+	        System.out.println(json);
+	    }
+	}
+
