445ff247519e3d6013d00c4d6dc0d450a6ef8d3f
Suyash3385 <suyashsharma942@gmail.com>
2026-01-30 16:12:16 +0530

    [Suyash]:Added code of e commerce

---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/lambdaexpression/ecommerce/OnlineShop.java
@@ -0,0 +1,28 @@
+package com.lambdaexpression.ecommerce;
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.List;
+	public class OnlineShop {
+	    public static void main(String[] args) {
+	        List<Product> products=new ArrayList<>();
+	        products.add(new Product( 60000, 5, 10));
+	        products.add(new Product( 30000, 3, 15));
+	        products.add(new Product( 2000, 4, 25));
+	        Collections.sort(products,(p1, p2) -> Double.compare(p1.getPrice(), p2.getPrice()));
+	        System.out.println("🔹 Sorted by Price:");
+	        print(products);
+	        Collections.sort(products,(p1, p2) -> Double.compare(p2.getRating(), p1.getRating()));
+	        System.out.println(" Sorted by Rating:");
+	        print(products);
+	        Collections.sort(products,(p1, p2) -> Double.compare(p2.getDiscount(), p1.getDiscount()));
+	        System.out.println(" Sorted by Discount:");
+	        print(products);
+	    }
+	    private static void print(List<Product>products) {
+	        for (Product p:products) {
+	            System.out.println(p);
+	        }
+	    }
+	}
+
+
---
+++ java8-programming-practice/gcr-codebase/Java8Programming/src/com/lambdaexpression/ecommerce/Product.java
@@ -0,0 +1,26 @@
+package com.lambdaexpression.ecommerce;
+
+public class Product {
+private int price;
+private int rating;
+private int discount;
+Product(int price,int rating,int discount){
+	this.price=price;
+	this.rating=rating;
+	this.discount=discount;
+	
+}
+public int getPrice() {
+	return price;
+	
+}
+public int getRating() {
+	return rating;
+}
+public int getDiscount() {
+	return discount;
+}
+public String toString() {
+	return "price : " + price + "rating :"+ rating+ "discount  :"+discount;
+}
+}
