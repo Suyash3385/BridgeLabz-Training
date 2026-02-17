00c97f49fac7f44ded72c40918b09956c231b9f4
Suyash3385 <suyashsharma942@gmail.com>
2026-02-17 13:32:03 +0530

    [Suyash]:Added bank transaction

---
+++ mysql-jdbc-practice/gcr-codebase/jdbcQuestions/src/main/java/com/JDBCPractice/bank_db/DatabaseConnection.java
@@ -0,0 +1,16 @@
+package com.JDBCPractice.bank_db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root@123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
